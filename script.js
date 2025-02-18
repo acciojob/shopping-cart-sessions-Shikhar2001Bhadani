699757b27107fedc15de12b4cefd54446049d441
Shikhar2001Bhadani <shkrbh30052001@gmail.com>
2025-02-18 21:21:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Product data
 const products = [
-  { id: 1, name: "Product 1", price: 10 },
-  { id: 2, name: "Product 2", price: 20 },
-  { id: 3, name: "Product 3", price: 30 },
-  { id: 4, name: "Product 4", price: 40 },
-  { id: 5, name: "Product 5", price: 50 },
+  {id:1,name:"Product 1",price:10},
+  {id:2,name:"Product 2",price:20},
+  {id:3,name:"Product 3",price:30},
+  {id:4,name:"Product 4",price:40},
+  {id:5,name:"Product 5",price:50},
 ];
 
 // DOM elements
@@ -18,7 +18,7 @@ function renderProducts() {
   products.forEach((product) => {
     const li = document.createElement("li");
     li.innerHTML = `
-      ${product.name} - $${product.price}
+      ${product.name}-$${product.price}
       <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>
     `;
     productList.appendChild(li);
@@ -39,7 +39,7 @@ function renderCart() {
 
   cart.forEach((product) => {
     const li = document.createElement("li");
-    li.innerHTML = `${product.name} - $${product.price}`;
+    li.innerHTML = `${product.name}-$${product.price}`;
     cartList.appendChild(li);
   });
 }
